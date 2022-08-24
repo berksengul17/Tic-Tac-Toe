936bd0c272a381d12f95e6c195a64b966714c30e
Berk Şengül <berksengul17@gmail.com>
2022-08-24 12:52:15 +0300

    Move lines in script.js to splitting.js

--- script.js
+++
@@ -1,6 +0,0 @@
-import { byWord } from "./splitting.js";
-
-const textContainer = document.querySelector(".game-start p");
-
-const nodes = byWord(textContainer.textContent);
-textContainer.firstChild.replaceWith(...nodes);
