90bb0c9d7ebdb0a965c3cfebecd78c9692044fb0
Berk Şengül <berksengul17@gmail.com>
2022-08-31 18:23:31 +0300

    Apply minimax algorithm for ai

--- script.js
+++ script.js
@@ -16,33 +16,127 @@ const GameBoard = (() => {
   const getMarkAt = (index) => _board[index];
   const setBoard = (index, mark) => (_board[index] = mark);
   const resetBoard = () => (_board = Array(9).fill(null));
+  const getEmptySpaces = (board = _board) => {
+    let emptySpaces = [];
+    for (let i = 0; i < 9; i++) {
+      if (board[i] === null) emptySpaces.push(i);
+    }
+    return emptySpaces;
+  };
 
   // If the marks in one of the combitions in the "_WINNING_PATTERNS" array
   // are the same return true otherwise return false
-  const checkForWin = (mark) =>
+  const checkForWin = (mark, board = _board) =>
     _WINNING_PATTERNS.some((pattern) =>
-      pattern.every((index) => _board[index] === mark)
+      pattern.every((index) => board[index] === mark)
     );
 
   // If none of the values are null then it is a draw
-  const checkForDraw = () => !_board.some((mark) => mark === null);
+  const checkForDraw = (board = _board) => !board.some((mark) => mark === null);
 
   return {
     getBoard,
     getMarkAt,
     setBoard,
     resetBoard,
+    getEmptySpaces,
     checkForWin,
     checkForDraw,
   };
 })();
 
-const Player = (name, mark) => {
+const Player = (name, mark, isAi) => {
   const getName = () => name;
   const getMark = () => mark;
+  const getIsAi = () => isAi;
 
-  return { getName, getMark };
+  return { getName, getMark, getIsAi };
 };
+
+const Minimax = (() => {
+  const _MAXIMIZER = "X";
+  const _MINIMIZER = "O";
+
+  // Return a score of 1 if x won -1 if o won
+  // 0 if none of them won
+  const _evaluate = (board) => {
+    if (GameBoard.checkForWin("X", board)) return 1;
+    else if (GameBoard.checkForWin("O", board)) return -1;
+    return 0;
+  };
+
+  // Evaluate all possible moves and return the value of the board
+  const _minimax = (board, depth, maximizingPlayer) => {
+    const score = _evaluate(board);
+
+    // If a player won return score
+    if (score == 1 || score == -1) return score;
+    // If draw return 0
+    if (GameBoard.checkForDraw(board)) return 0;
+
+    // Maximizing player's turn
+    if (maximizingPlayer) {
+      let maxEval = -Infinity;
+      // Traverse all cells
+      for (let i = 0; i < 9; i++) {
+        // If empty try it
+        if (board[i] === null) {
+          board[i] = _MAXIMIZER;
+          // Call the function recursively to find the maximum value
+          let eval = _minimax(board, depth + 1, false);
+          maxEval = Math.max(maxEval, eval);
+          // Undo the move
+          board[i] = null;
+        }
+      }
+      return maxEval;
+    }
+
+    // Minimizing player's turn
+    else {
+      let minEval = +Infinity;
+      // Traverse all cells
+      for (let i = 0; i < 9; i++) {
+        // If empty try it
+        if (board[i] === null) {
+          board[i] = _MINIMIZER;
+          // Call the function recursively to find the minimum value
+          let eval = _minimax(board, depth + 1, true);
+          minEval = Math.min(minEval, eval);
+          // Undo the move
+          board[i] = null;
+        }
+      }
+      return minEval;
+    }
+  };
+
+  // Return best possible move for the player
+  const findBestMove = (board) => {
+    let bestMove = null;
+    let bestEval = +Infinity;
+
+    // Traverse all cells, evaluate
+    // minimax function for all empty
+    // cells. And return the cell
+    // with optimal value.
+    for (let i = 0; i < 9; i++) {
+      if (board[i] === null) {
+        board[i] = _MINIMIZER;
+        let eval = _minimax(board, 0, true);
+        board[i] = null;
+
+        if (eval < bestEval) {
+          bestEval = eval;
+          bestMove = i;
+        }
+      }
+    }
+    return bestMove;
+  };
+
+  return { findBestMove };
+})();
 
 const DisplayController = (() => {
   const drawMark = (index) => {
@@ -80,9 +174,9 @@ const GameController = (() => {
       'input[name="opponent"]:checked'
     ).id;
     if (opponent === "ai") {
-      player_2 = Player("AI", "O");
+      player_2 = Player("AI", "O", true);
     } else if (opponent === "human") {
-      player_2 = Player("Player 2", "O");
+      player_2 = Player("Player 2", "O", false);
     }
     currentTurn = player_1.getMark();
   };
@@ -115,6 +209,23 @@ const GameController = (() => {
     resetGame();
   };
 
+  // Get the best index and the html element with that index
+  // then draw the mark on that index and remove the event listener from that element
+  // to prevent the player from making any changes
+  const makeAiMove = () => {
+    let index = Minimax.findBestMove(GameBoard.getBoard());
+    const element = document.querySelector(`[data-index="${index}"]`);
+    GameBoard.setBoard(index, currentTurn);
+    DisplayController.drawMark(index);
+    element.removeEventListener("click", makeMove);
+
+    // If win or draw end game otherwise change turn
+    if (GameBoard.checkForWin(currentTurn))
+      endGame(`${player_2.getName()} wins!`);
+    else if (GameBoard.checkForDraw()) endGame("It's a draw!");
+    else changeTurn();
+  };
+
   // Draw the mark to the clicked space
   // check for game over if game over display
   // the game ove menu with the winner's name
@@ -135,6 +246,7 @@ const GameController = (() => {
       endGame("It's a draw!");
     } else {
       changeTurn();
+      if (player_2.getIsAi()) makeAiMove();
     }
   };
 
@@ -155,6 +267,7 @@ const GameController = (() => {
     else currentTurn = player_1.getMark();
   };
 
+  // Add event listeners
   startBtn.addEventListener("click", startGame);
   restartBtns.forEach((restartBtn) =>
     restartBtn.addEventListener("click", resetGame)
